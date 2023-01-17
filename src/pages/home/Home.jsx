8dc111f7ef87501d91744381a7585314115f4cf9
santyrh70 <serodriguh@eafit.edu.co>
2023-01-17 05:45:14 -0500

    get data from API with Axios

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,17 +12,20 @@ const Home = () => {
 
   const { apiImages, setApiImages } = useContext(Context);
   const [imageArray, setImageArray] = useState(images);
+  const [searchValue, setSearchValue] = useState('');
   const withoutResMssg = <h1>{texts.WITHOUT_RESULTS}</h1>
 
   const handleSearchChange = (e) => {
     if (e.key === "Enter") {
-      setImageArray(filterArray(images, e.target.value));
+      setImageArray(filterArray(images, e.target.value)); 
     }
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
   }
 
   const renderImages = () => {
     return imageArray.map(
-      (image) => <Card imgData={image} />
+      (image) => <Card imgData={image} key={image.id}/>
     );
   }
 
@@ -32,12 +35,14 @@ const Home = () => {
   
 
   const callService = async () => {
+
+    //put this in handleSearchValue
     try {
       //const response = await fetch(`${pexelsApi}/search?query=nature&per_page=1`, { method: 'GET', headers: { Authorization: process.env.REACT_APP_PEXELS_API_KEY } });
       //const data = await response.json();
       //console.log(data.photos);
-
-      const { data } = await pexelsApiAxios.get('/search?query=nature&per_page=10', {
+      console.log(searchValue);
+      const { data } = await pexelsApiAxios.get(`/search?query=${searchValue}&per_page=5`, {
         headers: { 'Authorization': process.env.REACT_APP_PEXELS_API_KEY }
       });
 
